8042effd61861bf77c48c5eea5ec9f9901fbf7e3
SrRenho <juanjogvizioli@gmail.com>
2025-09-08 16:25:42 -0300

    borders

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -8,29 +8,34 @@ export default function Leaderboard({ night, metadata }) {
   const players = [...night.records].sort((a, b) => a.rank - b.rank);
 
   return (
-    <div className="leaderboard">
-      <div className="row header smaller" style={{marginBottom: "1rem"}}>
-        <span className="player-cell"/>
-        <span style={{ fontStyle: "italic", width: "12rem"}}>
-          Noche{" "}<AnimatedNight nightNumber={nightNumber}/>
-        </ span >
-        <span style={{width: "2rem"}}  />
-        <span style={{width: "12rem"}}>GLOBAL</span>
-        <span style={{width: "4rem", marginLeft: "1.75rem"}}>GAMES</span>
-        <span  style={{width: "10.5rem"}}>ÚLTIMAS</span>
+    <div className="grid-wrapper">
+      <div className="submatrix1-border" />
+      <div className="submatrix2-border" />
+      <div className="leaderboard">
+        <div className="row header smaller" style={{marginBottom: "1rem"}}>
+          <span className="player-cell"/>
+          <span style={{ fontStyle: "italic", width: "12rem"}}>
+            Noche{" "}<AnimatedNight nightNumber={nightNumber}/>
+          </ span >
+          <span style={{width: "2rem"}}  />
+          <span style={{width: "12rem"}}>GLOBAL</span>
+          <span style={{width: "4rem", marginLeft: "1.75rem"}}>GAMES</span>
+          <span  style={{width: "10.5rem"}}>ÚLTIMAS</span>
+        </div>
+
+        <div className="rows">
+          {players.map((r) => (
+            <motion.div key={r.player} layout transition={{ layout: { type: "spring", stiffness: 50, damping: 20 } }}>
+              <PlayerRow
+                {...r}
+                color={metadata.color[r.player]}
+                animal={metadata.animal[r.player]}
+              />
+            </motion.div>
+          ))}
+        </div>
       </div>
 
-      <div className="rows">
-        {players.map((r) => (
-          <motion.div key={r.player} layout transition={{ layout: { type: "spring", stiffness: 50, damping: 20 } }}>
-            <PlayerRow
-              {...r}
-              color={metadata.color[r.player]}
-              animal={metadata.animal[r.player]}
-            />
-          </motion.div>
-        ))}
-      </div>
     </div>
   );
 }
